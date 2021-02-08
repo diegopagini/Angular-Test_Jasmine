6a9fbbf30e681f3c0abac4771d046e09fd2f1409
Diego Pagini <dq@qbkconsulting.com>
2021-02-08 08:28:18 -0300

    probando strings

---
+++ src/app/basicas/string/string.spec.ts
@@ -0,0 +1,22 @@
+import { mensaje } from './string';
+
+//Agrupa pruebas
+// describe('Pruebas de Strings');
+
+//Evaluacion en concreto
+// it('Debe de regresar un string');
+
+describe('Pruebas de strings', () => {
+  it('Debe de regresar un string', () => {
+    const resp = mensaje('Diego');
+
+    expect(typeof resp).toBe('string');
+  });
+
+  it('Debe de retornar un saludo con el nombre enviado', () => {
+    const nombre = 'Juan';
+    const resp = mensaje(nombre);
+
+    expect(resp).toContain(nombre);
+  });
+});
---
+++ src/app/basicas/string/string.ts
@@ -0,0 +1,3 @@
+export function mensaje(nombre: string) {
+  return `Saludos ${nombre}`; //Saludos nombre
+}
