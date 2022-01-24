99c0197075a2357e5354f9428319fb7b43756300
Diego Pagini <diegopaginidev@gmail.com>
2022-01-24 12:53:36 +0100

    update

--- src/app/avanzado/router-medico/router-medico.component.spec.ts
+++ src/app/avanzado/router-medico/router-medico.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { RouterMedicoComponent } from './router-medico.component';
 import { Router, ActivatedRoute } from '@angular/router';
@@ -27,7 +27,7 @@ describe('RouterMedicoComponent', () => {
   let component: RouterMedicoComponent;
   let fixture: ComponentFixture<RouterMedicoComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [RouterMedicoComponent],
       providers: [
